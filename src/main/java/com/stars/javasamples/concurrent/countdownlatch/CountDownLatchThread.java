cf74976d1572d174b7e9e30207ccfde1ff38e7d6
Guilin <guilinw98@gmail.com>
2023-04-06 09:17:10 +0800

    Initial commit

---
+++ src/main/java/com/stars/javasamples/concurrent/countdownlatch/CountDownLatchThread.java
@@ -0,0 +1,31 @@
+package com.stars.javasamples.concurrent.countdownlatch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CountDownLatch;
+
+public class CountDownLatchThread extends Thread {
+	private static final Logger logger = LoggerFactory.getLogger(CountDownLatchThread.class);
+	private String name;
+	private CountDownLatch latch;
+
+	public CountDownLatchThread(String name, CountDownLatch latch) {
+		this.name = name;
+		this.latch = latch;
+	}
+
+	@Override
+	public void run() {
+		try {
+			Thread.sleep((long) (10000 * Math.random()));
+			latch.countDown();
+			logger.info("CountDownLatch finish" + name);
+			// 执行完可继续往下运行
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
